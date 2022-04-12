def3046dd3159310eca511fbc38ef0103f237b8b
Harsh Patel <harshp1898@gmail.com>
2022-04-11 21:26:19 -0400

    Added the method BFS in order to compute the shortest path

--- src/BFS.java
+++ src/BFS.java
@@ -27,6 +27,38 @@ public class BFS {
     }
     private static boolean BFS(ArrayList<ArrayList<Integer>> adj, int src, int dest, int v, int pred[], int dist[])
     {
-    return false;
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+
+        boolean visited[] = new boolean[v];
+
+        for (int i = 0; i < v; i++) {
+            visited[i] = false;
+            dist[i] = Integer.MAX_VALUE;
+            pred[i] = -1;
+        }
+
+        visited[src] = true;
+        dist[src] = 0;
+        queue.add(src);
+
+        // bfs Algorithm
+        while (!queue.isEmpty()) {
+            int u = queue.remove();
+
+            for (int i = 0; i < adj.get(u).size(); i++) {
+
+                if (visited[adj.get(u).get(i)] == false) {
+
+                    visited[adj.get(u).get(i)] = true;
+                    dist[adj.get(u).get(i)] = dist[u] + 1;
+                    pred[adj.get(u).get(i)] = u;
+                    queue.add(adj.get(u).get(i));
+
+                    if (adj.get(u).get(i) == dest)
+                        return true;
+                }
+            }
+        }
+        return false;
     }
 }
