7e8f37fff98afa958cabb1e0c3060f19f6cd263d
Harsh Patel <harshp1898@gmail.com>
2022-04-13 23:03:08 -0400

    Added the printGraph method

--- src/GraphReader.java
+++ src/GraphReader.java
@@ -5,4 +5,16 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GraphReader {
+    public static void printGraph(ArrayList<ArrayList<Integer>> adj) {
+        int count = 0;
+        for (ArrayList<Integer> a : adj) {
+            System.out.print(count + " --> ");
+            for (int aa : a)
+                System.out.print(aa + " ");
+            count++;
+            System.out.println();
+        }
+
+    }
+
 }
